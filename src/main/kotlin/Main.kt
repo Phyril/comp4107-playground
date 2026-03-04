9ff7a72b456eee42935a121151e26b0d50022a3b
Phyril <25223909@life.hkbu.edu.hk>
2026-03-04 22:49:31 +0800

    Quick trunk-based change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,4 @@ fun main() {
 //    println("Exercise 1b: Trunk-Based Development – Short-Lived Branch or Direct Commit")
 //    println("Option B – Short-lived branch")
 
-//    for (i in 1..5) {
-//        println("i = $i")
-//    }
 }
